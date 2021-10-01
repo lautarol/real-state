aaf15a2ec0105ea6e4d18b180afaaf6294b27fc2
Lautaro Larroucau <llarroucau@gmail.com>
2021-10-01 16:26:45 -0300

    add property migration

--- Gemfile
+++ Gemfile
@@ -36,5 +36,10 @@ group :development do
   gem 'spring'
 end
 
+group :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ db/migrate/20211001182705_create_properties.rb
@@ -0,0 +1,17 @@
+class CreateProperties < ActiveRecord::Migration[6.1]
+  def change
+    create_table :properties do |t|
+      t.string :name, null: false, limit: 128
+      t.integer :type, null: false
+      t.string :street, null: false, limit: 128
+      t.string :external_number, null: false, limit: 12
+      t.string :internal_number, limit: 12
+      t.string :neighborhood, null: false, limit: 128
+      t.string :city, null: false, limit: 64
+      t.string :country, null: false, limit: 2
+      t.integer :rooms, null: false
+      t.float :bathrooms, null: false
+      t.timestamps
+    end
+  end
+end
